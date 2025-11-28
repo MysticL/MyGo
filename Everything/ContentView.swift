917bcd9516ccaac9bb0056d7c6660feeb7f7f672
MysticL <1302375877@qq.com>
2025-11-28 17:22:59 +0800

    Initial Commit

---
+++ Everything/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Everything
+//
+//  Created by b-60060526 on 11/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Everything/EverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverythingApp.swift
+//  Everything
+//
+//  Created by b-60060526 on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct EverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
